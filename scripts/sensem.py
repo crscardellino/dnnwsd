99d2119d14158a4fec53be73373aae9bcd625030
Cristian Cardellino <crscardellino@gmail.com>
2015-09-25 09:35:34 -0300

    Script to clean sensem from xml

---
+++ scripts/sensem.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+from bs4 import BeautifulSoup
+from lxml import etree
+
+ifile = sys.argv[1]
+ofile = sys.argv[2]
+
+tree = etree.parse(ifile)
+
+with open(ofile, 'w') as outf:
+    for sentence in tree.findall('.//content'):
+        ofile.write(re.sub(r'\s\s+', ' ', BeautifulSoup(
+            etree.tostring(sentence, encoding='UTF-8'), 'lxml'
+        ).get_text().replace('\n', ' ').strip()).encode('utf-8'))
