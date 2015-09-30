7acda36a7450a23fcfd5af07ee8969c533baa78f
Cristian Cardellino <crscardellino@gmail.com>
2015-09-30 16:11:40 -0300

    Rewrite the clean_corpus script to use a newline for each sentence

--- scripts/clean_corpus.py
+++ scripts/clean_corpus.py
@@ -13,17 +13,24 @@ with open(ifile, 'r') as inf:
             if line.strip().startswith("<doc ") or line.strip() == "</doc>" or line.strip() == "":
                 continue
 
-            outl = re.sub(ur'[\W_]+', u' ', line.decode('utf-8').strip(), flags=re.UNICODE).strip()
-            outl = outl.replace('0', 'cero ').strip()
-            outl = outl.replace('1', 'uno ').strip()
-            outl = outl.replace('2', 'dos ').strip()
-            outl = outl.replace('3', 'tres ').strip()
-            outl = outl.replace('4', 'cuatro ').strip()
-            outl = outl.replace('5', 'cinco ').strip()
-            outl = outl.replace('6', 'seis ').strip()
-            outl = outl.replace('7', 'siete ').strip()
-            outl = outl.replace('8', 'ocho ').strip()
-            outl = outl.replace('9', 'nueve ').strip()
+            sentences = re.sub(r'\.\s+|\.$', '\n', line.decode('utf-8').strip(), flags=re.UNICODE).strip().split('\n')
 
-            if outl != "":
-                outf.write(outl.encode('utf-8') + '\n')
+            for idx, sentence in enumerate(sentences, start=1):
+                outl = re.sub(r'[\W_]+', ' ', sentence, flags=re.UNICODE).strip()
+                outl = outl.replace('0', 'cero ').strip()
+                outl = outl.replace('1', 'uno ').strip()
+                outl = outl.replace('2', 'dos ').strip()
+                outl = outl.replace('3', 'tres ').strip()
+                outl = outl.replace('4', 'cuatro ').strip()
+                outl = outl.replace('5', 'cinco ').strip()
+                outl = outl.replace('6', 'seis ').strip()
+                outl = outl.replace('7', 'siete ').strip()
+                outl = outl.replace('8', 'ocho ').strip()
+                outl = outl.replace('9', 'nueve ').strip()
+                outl = re.sub(r'\s+', ' ', outl, flags=re.UNICODE)  # Remove double spaces
+
+                outf.write(outl.encode('utf-8'))
+                if idx != len(sentences):
+                    outf.write('\n')
+                else:
+                    outf.write(' ')
