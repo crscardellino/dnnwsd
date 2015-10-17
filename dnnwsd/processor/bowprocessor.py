12a32a4f7b88a4108b8cfc217a7b3c7ad2a6648d
Cristian Cardellino <crscardellino@gmail.com>
2015-10-17 12:02:08 -0300

    Compated the processors classes
    Save a lot of code by using better abstractions for the processors
    classes. Also, check on some errors they had.

--- dnnwsd/processor/bowprocessor.py
+++ dnnwsd/processor/bowprocessor.py
@@ -3,6 +3,7 @@
 import logging
 import numpy as np
 from collections import defaultdict
+from scipy import sparse
 from .base import BaseProcessor
 from ..utils.setup_logging import setup_logging
 
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 class BoWProcessor(BaseProcessor):
-    def __init__(self, corpus, vocabulary_filter=5, window_size=2):
+    def __init__(self, corpus, vocabulary_filter=2, window_size=2):
         super(BoWProcessor, self).__init__(corpus)
         self.vocabulary_filter = vocabulary_filter
         self.window_size = window_size
@@ -31,22 +32,37 @@ class BoWProcessor(BaseProcessor):
         self.features = \
             np.sort(np.array([word for word, count in vocabulary.iteritems() if count >= self.vocabulary_filter]))
 
+    def _get_instance_data(self, sentence):
+        return np.array([word.token for word in sentence.predicate_window(self.window_size)])
+
     def instances(self):
         dataset = []
         target = []
 
         for sentence in self.corpus:
-            window = np.array([word.token for word in sentence.predicate_window(self.window_size)])  # Get the BoW set
-            bow = np.searchsorted(self.features, window)  # Get the indices of every word in the vocabulary
-            dataset.append(np.bincount(bow).astype(np.int32))  # Counts the amount of words
+            # Get the data (depends on the processor's class)
+            instance_features = self._get_instance_data(sentence)
+
+            # Get the indices of every word in the vocabulary (that was not filtered)
+            instance_features = np.searchsorted(
+                self.features, instance_features[np.in1d(instance_features, self.features)]
+            )
+
+            # Counts the amounts
+            instance_features = np.bincount(instance_features).astype(np.int32)
+
+            # Resize to match the features shape
+            instance_features.resize(self.features.shape)
+
+            dataset.append(instance_features)
             target.append(self.labels.index(sentence.sense))
 
-        self.dataset = np.vstack(dataset)
+        self.dataset = sparse.csr_matrix(np.vstack(dataset))
         self.target = np.array(target, dtype=np.int32)
 
 
 class BoPoSProcessor(BoWProcessor):
-    def __init__(self, corpus, vocabulary_filter=5, window_size=2, pos_filter=0):
+    def __init__(self, corpus, vocabulary_filter=2, window_size=2, pos_filter=2):
         self.pos_filter = pos_filter
         super(BoPoSProcessor, self).__init__(corpus, vocabulary_filter, window_size)
 
@@ -59,8 +75,8 @@ class BoPoSProcessor(BoWProcessor):
         postags = defaultdict(int)
 
         for sentence in self.corpus:
-            for word in sentence.predicate_window(self.window_size):
-                postags[word.tag] += 1
+            for widx, word in enumerate(sentence.predicate_window(self.window_size)):
+                postags[self._get_tag_literal(word, widx)] += 1
 
         logger.info(u"Filtering the part of speech tags of the corpus from lemma {}"
                     .format(self.corpus.lemma).encode("utf-8"))
@@ -71,16 +87,22 @@ class BoPoSProcessor(BoWProcessor):
                 np.sort(np.array([pos for pos, count in postags.iteritems() if count >= self.pos_filter]))
             ))
 
-    def instances(self):
-        dataset = []
-        target = []
+    def _get_tag_literal(self, *args):
+        return args[0].tag
 
-        for sentence in self.corpus:
-            window = np.array([word.token for word in sentence.predicate_window(self.window_size)])
-            window = np.hstack((window, [word.tag for word in sentence.predicate_window(self.window_size)]))
-            bow = np.searchsorted(self.features, window)
-            dataset.append(np.bincount(bow).astype(np.int32))
-            target.append(self.labels.index(sentence.sense))
+    def _get_instance_data(self, sentence):
+        instance_data = np.array([word.token for word in sentence.predicate_window(self.window_size)])
 
-        self.dataset = np.vstack(dataset)
-        self.target = np.array(target, dtype=np.int32)
+        tags_literal = map(
+            lambda (widx, word): self._get_tag_literal(word, widx),
+            enumerate(sentence.predicate_window(self.window_size))
+        )
+
+        instance_data = np.hstack((instance_data, tags_literal))
+
+        return instance_data
+
+
+class PoSProcessor(BoPoSProcessor):
+    def _get_tag_literal(self, *args):
+        return u"{}{:+d}".format(args[0].tag, args[1]-self.window_size)
