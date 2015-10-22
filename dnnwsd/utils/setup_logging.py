7f0eed4220b8e60fd5eaf21a61c4aea1da4b5719
Cristian Cardellino <crscardellino@gmail.com>
2015-10-22 14:35:40 -0300

    Moved the setup logging configuration file

--- dnnwsd/utils/setup_logging.py
+++ dnnwsd/utils/setup_logging.py
@@ -5,9 +5,11 @@ import yaml
 
 from os import path
 
+CONFIG_FILE = u"config/logging.yaml"
+
 
 def setup_logging():
-    file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.yaml')
+    file_path = path.join(CONFIG_FILE)
     with open(file_path, 'rt') as f:
         config = yaml.load(f.read())
     logging.config.dictConfig(config)
