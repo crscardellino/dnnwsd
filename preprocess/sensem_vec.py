aa8f73c3b76592c91d825ebe3e7747fcb9503253
Cristian Cardellino <crscardellino@gmail.com>
2016-02-27 12:13:50 -0300

    Semeval and Sensem vec preprocessors

--- preprocess/sensem_vec.py
+++ preprocess/sensem_vec.py
@@ -21,8 +21,8 @@ from dnnwsd.corpus import sensem, unannotated
 from dnnwsd.processor import vecprocessor
 
 annotated_corpus_directory = "../resources/sensem/"
-unannotated_corpus_directory = "../../wikicorpus/es/wikicorpus_lemmas_sample_30k/"
-corpus_datasets_directory = "../resources/corpus_datasets/"
+unannotated_corpus_directory = "../../wikicorpus/es/wikicorpus_lemmas_sample_15k/"
+corpus_datasets_dir = "../resources/corpus_datasets/es/15k/vec"
 
 annotated_corpus_directory_iterator = sensem.SenSemCorpusDirectoryIterator(annotated_corpus_directory)
 unannotated_corpus_directory_iterator = unannotated.UnannotatedCorpusDirectoryIterator(unannotated_corpus_directory)
@@ -31,52 +31,46 @@ word_vectors_path = "../resources/wordvectors/SBW-vectors-300-min5.bin.gz"
 
 word2vec_model = gensim.models.Word2Vec.load_word2vec_format(word_vectors_path, binary=True)
 
-part = 0
+for corpus_index, annotated_corpus in enumerate(annotated_corpus_directory_iterator):
+    if not annotated_corpus.has_multiple_senses() or annotated_corpus.lemma == u'estar':
+        print u"Skipping preprocess for corpus of lemma {}".format(annotated_corpus.lemma)
+        continue
 
-for start, end in zip(range(0, 250, 25), range(25, 275, 25)):
-    vec_datasets = {}
+    unannotated_corpus = unannotated_corpus_directory_iterator[annotated_corpus.lemma]
 
-    for corpus_index, annotated_corpus in enumerate(annotated_corpus_directory_iterator):
-        if not (start <= corpus_index < end):
-            continue
+    vec_processor = vecprocessor.SemiSupervisedWordVectorsProcessor(
+        annotated_corpus, unannotated_corpus, word2vec_model)
 
-        if not annotated_corpus.has_multiple_senses() or annotated_corpus.lemma == u'estar':
-            print u"Skipping preprocess for corpus of lemma {}".format(annotated_corpus.lemma)
-            continue
+    vec_processor.instances()
 
-        unannotated_corpus = unannotated_corpus_directory_iterator[annotated_corpus.lemma]
-        
-        vec_processor = vecprocessor.SemiSupervisedWordVectorsProcessor(
-            annotated_corpus, unannotated_corpus, word2vec_model)
+    annotated_dataset = dict(data=vec_processor.dataset, target=vec_processor.target)
+    sentences_ids = []
+    unannotated_sentences = {}
 
-        vec_processor.instances()
-        
-        annotated_dataset = dict(data=vec_processor.dataset, target=vec_processor.target)
-        sentences_ids = []
-        unannotated_sentences = {}
-        
-        for sentence in unannotated_corpus:
-            sentence_id = str(uuid.uuid4())
-            raw_sentence = []
-            
-            for word in sentence:
-                word_token = u"_{}_".format(word.token) if word.is_main_lemma else word.token
-                raw_sentence.append(word_token)
-            
-            raw_sentence = " ".join(raw_sentence)
-            
-            sentences_ids.append(sentence_id)
-            unannotated_sentences[sentence_id] = raw_sentence
+    for sentence in unannotated_corpus:
+        sentence_id = str(uuid.uuid4())
+        raw_sentence = []
 
-        unannotated_dataset = dict(data=vec_processor.unannotated_dataset, sentences=np.array(sentences_ids))
-        
-        vec_datasets["{:03d}".format(corpus_index)] = dict(
-            annotated_dataset=annotated_dataset,
-            unannotated_dataset=unannotated_dataset,
-            unannotated_sentences=unannotated_sentences
-        )
+        for word in sentence:
+            word_token = u"_{}_".format(word.token) if word.is_main_lemma else word.token
+            raw_sentence.append(word_token)
 
-    corpus_datasets = path.join(corpus_datasets_directory, "vec_datasets_es_{}.p".format(part))
-    part += 1
-    with open(corpus_datasets, "wb") as f:
-        pickle.dump(vec_datasets, f)
+        raw_sentence = " ".join(raw_sentence)
+
+        sentences_ids.append(sentence_id)
+        unannotated_sentences[sentence_id] = raw_sentence
+
+    unannotated_dataset = dict(data=vec_processor.unannotated_dataset, sentences=np.array(sentences_ids))
+
+    lemma_dataset = dict(
+        lemma=annotated_corpus.lemma,
+        index="{:03d}".format(corpus_index),
+        annotated_dataset=annotated_dataset,
+        unannotated_dataset=unannotated_dataset,
+        unannotated_sentences=unannotated_sentences
+    )
+
+    corpus_dataset = path.join(corpus_datasets_dir, "{:03d}".format(corpus_index))
+
+    with open(corpus_dataset, "wb") as f:
+        pickle.dump(lemma_dataset, f)
